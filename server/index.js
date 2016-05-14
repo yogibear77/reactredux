bfc15bd514add70bfc8e22cac97f4c92293386cb
yogibear77 <bhavanb@gmail.com>
2016-05-14 15:30:27 +0000

    setup server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const PORT = 8080;
+
+http.createServer((req, res) => {
+    console.log(`Received request for ${req.url}`);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World!\n');
+    res.end(`Received request for ${req.url}`);
+  })
+
+.listen(PORT, () => {
+    console.log(`Server running at 172.17.1.108:${PORT}/`);
+  });
+
+    
